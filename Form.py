dcf879ab397fb9630bf072d56673b72dc8971eaa
SSG0513 <hirokimapplicant@gmail.com>
2023-07-23 18:53:55 +0900

    tougou

---
+++ Form.py
@@ -0,0 +1,51 @@
+import os
+import json
+from git import Repo
+
+def get_input():
+    # Get input from user
+    description = input("Enter description: ")
+    icon = input("Enter icon: ")
+    coordinates = input("Enter coordinates (comma-separated): ").split(',')
+
+    # Convert coordinates to float
+    coordinates = [float(x.strip()) for x in coordinates]
+
+    return description, icon, coordinates
+
+def main():
+    # Get input from user
+    description, icon, coordinates = get_input()
+
+    # Load existing data
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+
+    # Add new data
+    new_data = {
+        'type': 'Feature',
+        'properties': {
+            'description': description,
+            'icon': icon
+        },
+        'geometry': {
+            'type': 'Point',
+            'coordinates': coordinates
+        }
+    }
+    data['features'].append(new_data)
+
+    # Save data
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+    # Commit and push changes
+    repo = Repo(os.getcwd())
+    repo.git.pull('origin', 'master')  # Pull latest changes from remote repository
+    repo.git.add('data.json')
+    repo.git.commit('-m', 'Add new data')
+    repo.git.push()
+
+
+if __name__ == "__main__":
+    main()
