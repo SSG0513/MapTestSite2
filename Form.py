c5e1c6509e277614b30aa842f430cdd77e0b2e77
SSG0513 <hirokimapplicant@gmail.com>
2023-07-25 10:01:46 +0900

    update

--- Form.py
+++ Form.py
@@ -21,7 +21,7 @@ def hard_reset():
 
     # Commit and push changes
     repo = Repo(os.getcwd())
-    repo.git.pull('origin', 'master')  # Pull latest changes from remote repository
+    repo.git.pull('origin', 'main')  # Pull latest changes from remote repository
     repo.git.add('data.json')
     repo.git.commit('-m', 'Hard reset')
     repo.git.push()
@@ -32,38 +32,7 @@ def main():
     if action == 'reset':
         hard_reset()
     else:
-        # Get input from user
-        description, icon, coordinates = get_input()
-
-        # Load existing data
-        with open('data.json', 'r') as f:
-            data = json.load(f)
-
-        # Add new data
-        new_data = {
-            'type': 'Feature',
-            'properties': {
-                'description': description,
-                'icon': icon
-            },
-            'geometry': {
-                'type': 'Point',
-                'coordinates': coordinates
-            }
-        }
-        data['features'].append(new_data)
-
-        # Save data
-        with open('data.json', 'w') as f:
-            json.dump(data, f)
-
-        # Commit and push changes
-        repo = Repo(os.getcwd())
-        repo.git.pull('origin', 'main')  # Pull latest changes from remote repository
-        repo.git.add('data.json')
-        repo.git.commit('-m', 'Add new data')
-        repo.git.push()
-
+    in
 
 if __name__ == "__main__":
     main()
